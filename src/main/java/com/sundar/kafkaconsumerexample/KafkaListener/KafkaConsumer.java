d7324ba407df03d07b7db2b1e4dc9a4d0d99bee1
SundarMandali <53479392+SundarMandali@users.noreply.github.com>
2020-06-30 20:25:47 +0530

    Add files via upload

---
+++ src/main/java/com/sundar/kafkaconsumerexample/KafkaListener/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.sundar.kafkaconsumerexample.KafkaListener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+	
+	@KafkaListener(topics="kafka_example",groupId="group_id")
+	public void consume(String message) {
+		System.out.println("Consumed message: "+message);
+	}
+	
+
+}
+ 
